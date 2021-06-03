35b1bbb6f2491e45bad5489d56f3f642aee0846b
Peter Bruckner <peterbruckner08@gmail.com>
2021-06-03 23:39:13 +0930

    done

--- models/index.js
+++ models/index.js
@@ -6,12 +6,11 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
   Product.belongsTo(Category, {
-    foreignKey: 'product_id',
+
   });
 // Categories have many Products
   Category.hasMany(Product, {
-    foreignKey: 'category_id',
-    onDelete: 'CASCADE',
+    onDelete: 'CASCADE'
   });
 // Products belongToMany Tags (through ProductTag)
   Product.belongsToMany(Tag, {
